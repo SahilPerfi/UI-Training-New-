5023f1403b8267dd743d145e7276fd5cb9ae3dea
Sahil Dhoble <sahil.perficient@gmail.com>
2023-08-24 16:52:28 +0530

    UI training changes

---
+++ AEM UI Training Assignment/aem-guides-wknd-spa-1.0.3-React/ui.apps/src/main/content/jcr_root/apps/wknd-spa-react/clientlibs/clientlib-react/resources/precache-manifest.eb411dc00c211eebeede5166fdd154a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357ab5e54e52316be5f65b70f0e6301e",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "c839689741a46522e892",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/css/main.51d26ea8.chunk.css"
+  },
+  {
+    "revision": "60b515d80b143f201a2e",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/2.03b711bb.chunk.js"
+  },
+  {
+    "revision": "c7907f91cc1d59c9b9ce1abcce899c39",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/2.03b711bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c839689741a46522e892",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/main.5b33a491.chunk.js"
+  },
+  {
+    "revision": "9d0800bdca3115c11b65",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/runtime-main.22807818.js"
+  },
+  {
+    "revision": "19a5bdabd660fde16554c866e650eadc",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/media/icon-back.19a5bdab.svg"
+  }
+]);
